859180ba2e0c6cd249dec7f1708f700af5116e55
crapthings <crapthings@gmail.com>
2016-09-19 17:23:35 +0800

    test with vmware

--- commands/index.js
+++ commands/index.js
@@ -12,6 +12,7 @@ function attachRun (ctx) {
     var start = new Date().getTime()
     var session = nodemiral.session(config.host, { username: config.username, password: config.password }, { keepAlive: false })
     session.execute(this.cmd, function(err, code, logs) {
+      if (err) return console.log(err, logs)
       console.log(logs.stdout)
       var end = new Date().getTime()
       console.log(end - start + 'ms')
